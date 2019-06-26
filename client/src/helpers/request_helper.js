ad2c12ae0b682f9d767d49e027265b7a37258c90
Michael Forbes <michaelforbez@hotmail.co.uk>
2019-06-26 10:15:43 +0100

    wireframe form done

---
+++ client/src/helpers/request_helper.js
@@ -0,0 +1,40 @@
+const RequestHelper = function (url) {
+    this.url = url;
+  };
+  
+  RequestHelper.prototype.get = function () {
+    return fetch(this.url)
+      .then((response) => response.json())
+      .catch(console.error)
+  };
+  
+  RequestHelper.prototype.post = function(payload, headers) {
+    return fetch(this.url, {
+      method: 'POST',
+      body: JSON.stringify(payload),
+      headers: headers
+    })
+    .then(response => response.json())
+    .catch(console.error)
+  }
+  
+  RequestHelper.prototype.delete = function (id) {
+    return fetch(`${this.url}/${id}`, {
+      method: 'DELETE'
+    })
+      .then((response) => response.json())
+      .catch(console.error)
+  };
+  
+  RequestHelper.prototype.put = function (id, payload) {
+    return fetch(`${this.url}/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify(payload),
+      headers: {'Content-Type': 'application/json'}
+    })
+      .then((response) => response.json())
+      .catch(console.error)
+  }
+  
+  module.exports = RequestHelper;
+  
