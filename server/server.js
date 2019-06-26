a40aca95973f34d916bad32008e6f4693ce1e513
Michael Forbes <michaelforbez@hotmail.co.uk>
2019-06-26 16:59:45 +0100

    Spelling amended to fix mongodb

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
-const MongoClient = require('mongo').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 const path = require('path');
 const createRouter = require('./helpers/create_router.js');
 
 const publicPath = path.join(__dirname, '../client/public');
 app.use(express.static(publicPath));
 
-MongoClient.connect('mongo://localhost:27017')
+MongoClient.connect('mongodb://localhost:27017')
     .then((client) => {
         const db = client.db('todoList');
         const toDoCollection = db.collection('toDo');
