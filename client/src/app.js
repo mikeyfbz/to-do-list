cc6766f3d58de2eeb6e1d2eda07f2901604eb02c
Michael Forbes <michaelforbez@hotmail.co.uk>
2019-06-27 21:58:51 +0100

    set date to default to today

--- client/src/app.js
+++ client/src/app.js
@@ -4,6 +4,9 @@ const GridView = require('./views/grid_view.js');
 
 document.addEventListener('DOMContentLoaded', function(){
 
+    let today = new Date().toISOString().substr(0, 10);
+    document.querySelector("#due_date").value = today;
+
     const completedList = document.querySelector('#completed')
     const toDoList = document.querySelector('#to_do_list');
     const gridView = new GridView(toDoList, completedList);
