56fd9dd0f5d060c4bcda3f003816f2d44ac66f61
Michael Forbes <michaelforbez@hotmail.co.uk>
2019-06-28 09:46:46 +0100

    Refactor of TileView Started

--- client/src/views/form_view.js
+++ client/src/views/form_view.js
@@ -10,6 +10,8 @@ FormView.prototype.bindEvents = function(){
         const details = event.target;
         PubSub.publish('FormView:entered-details', details);
         event.target.reset();
+        let today = new Date().toISOString().substr(0, 10);
+        document.querySelector("#due_date").value = today;
     })
 }
 
--- client/src/views/tile_view.js
+++ client/src/views/tile_view.js
@@ -8,117 +8,77 @@ const TileView = function(element, completedElement){
 
 TileView.prototype.render = function(tile, index){
     if(tile.completed == false){
-        const newTile = document.createElement('div');
-        newTile.classList.add('tile');
-        newTile.id = `tile${tile._id}`;
-
-        const titleLable = document.createElement('label');
-        titleLable.textContent = "Title: ";
-        const title = document.createElement('h3');
-        title.textContent = tile.title;
-        newTile.appendChild(titleLable);
-        newTile.appendChild(title);
-
-        const dateLabel = document.createElement('label');
-        dateLabel.textContent = "Date: ";
-        const date = document.createElement('h3');
-        date.textContent = tile.due_date;
-        newTile.appendChild(dateLabel);
-        newTile.appendChild(date);
-
-        const descLabel = document.createElement('label');
-        descLabel.textContent = "Description: ";
-        const desc = document.createElement('h3');
-        desc.textContent = tile.desc;
-        desc.classList.add('desc');
-        newTile.appendChild(descLabel);
-        newTile.appendChild(desc);
-
-        const buttons = document.createElement('div');
-        buttons.classList.add('buttons');
-        newTile.appendChild(buttons);
-
-        const completedButton = document.createElement('button');
-        completedButton.textContent = "Completed";
-        completedButton.id = `completedButton${index}`;
-        completedButton.value = tile._id;
-        buttons.appendChild(completedButton);
-
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = "Delete";
-        deleteButton.id = `deleteButton${index}`;
-        deleteButton.value = tile._id;
-        buttons.appendChild(deleteButton);
-
-        this.element.appendChild(newTile);
-
-        deleteButton.addEventListener('click', (event)=> {
-            const click = event.target.value;
-            PubSub.publish('TileView:delete-tile', click)
-        })
+        this.populateTile(tile, index, this.element)
         
-        completedButton.addEventListener('click', (event)=>{
-            const click = event;
-            PubSub.publish('TileView:tile-complete', click);
-        })
     } else {
-        const newTile = document.createElement('div');
-        newTile.classList.add('tile');
-        newTile.id = `tile${tile._id}`;
-
-        const titleLable = document.createElement('label');
-        titleLable.textContent = "Title: ";
-        const title = document.createElement('h3');
-        title.textContent = tile.title;
-        newTile.appendChild(titleLable);
-        newTile.appendChild(title);
-
-        const dateLabel = document.createElement('label');
-        dateLabel.textContent = "Date: ";
-        const date = document.createElement('h3');
-        date.textContent = tile.due_date;
-        newTile.appendChild(dateLabel);
-        newTile.appendChild(date);
-
-        const descLabel = document.createElement('label');
-        descLabel.textContent = "Description: ";
-        const desc = document.createElement('h3');
-        desc.textContent = tile.desc;
-        desc.classList.add('desc');
-        newTile.appendChild(descLabel);
-        newTile.appendChild(desc);
-
-        const buttons = document.createElement('div');
-        buttons.classList.add('buttons');
-        newTile.appendChild(buttons);
-
-        const completedButton = document.createElement('button');
-        completedButton.textContent = "Completed";
-        completedButton.id = `completedButton${index}`;
-        completedButton.value = tile._id;
-        buttons.appendChild(completedButton);
-
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = "Delete";
-        deleteButton.id = `deleteButton${index}`;
-        deleteButton.value = tile._id;
-        buttons.appendChild(deleteButton);
-
-        this.completedElement.appendChild(newTile);
-
-        deleteButton.addEventListener('click', (event)=> {
-            const click = event.target.value;
-            PubSub.publish('TileView:delete-tile', click)
-        })
-        
-        completedButton.addEventListener('click', (event)=>{
-            const click = event;
-            PubSub.publish('TileView:tile-complete', click);
-        })
+        this.populateTile(tile, index, this.completedElement)
 
     }
 }
 
+TileView.prototype.populateTile = function(tile, index, element){
+    const newTile = document.createElement('div');
+        newTile.classList.add('tile');
+        newTile.id = `tile${tile._id}`;
 
+        const titleLable = this.createLabel("Title: ")
+        const title = this.createContent(tile.title);
+        newTile.appendChild(titleLable);
+        newTile.appendChild(title);
+
+        const dateLabel = this.createLabel("Date: ")
+        const date = this.createContent(tile.due_date);
+        newTile.appendChild(dateLabel);
+        newTile.appendChild(date);
+
+        const descLabel = this.createLabel("Description: ");
+        const desc = this.createContent(tile.desc);
+        desc.classList.add('desc');
+        newTile.appendChild(descLabel);
+        newTile.appendChild(desc);
+
+        const buttons = document.createElement('div');
+        buttons.classList.add('buttons');
+        newTile.appendChild(buttons);
+
+        const completedButton = document.createElement('button');
+        completedButton.textContent = "Completed";
+        completedButton.id = `completedButton${index}`;
+        completedButton.value = tile._id;
+        if(tile.completed == true){
+            completedButton.classList.add('hide');
+        }
+        buttons.appendChild(completedButton);
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = "Delete";
+        deleteButton.id = `deleteButton${index}`;
+        deleteButton.value = tile._id;
+        buttons.appendChild(deleteButton);
+
+        element.appendChild(newTile);
+
+        deleteButton.addEventListener('click', (event)=> {
+            const click = event.target.value;
+            PubSub.publish('TileView:delete-tile', click)
+        })
+        
+        completedButton.addEventListener('click', (event)=>{
+            const click = event;
+            PubSub.publish('TileView:tile-complete', click);
+        })
+}
+
+TileView.prototype.createLabel= function(labelContent){
+    const label = document.createElement('label');
+    label.textContent = labelContent;
+    return label;
+}
+
+TileView.prototype.createContent = function(content){
+    const item = document.createElement('h3');
+    item.textContent = content;
+    return item;
+}
 
 module.exports = TileView;
